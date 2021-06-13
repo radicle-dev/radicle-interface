0b9d40a1aaecaf98ab181146b20a842668dcf87f
Sebastian Martinez <sebastinez@me.com>
2021-06-13 20:26:16 -0300

    Add CI to check code format
    Signed-off-by: Sebastian Martinez <sebastinez@me.com>

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: [
+    "svelte3",
+    "@typescript-eslint"
+  ],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    }
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    indent: ["error", 2, { "SwitchCase": 1 }]
+  },
+  settings: {
+    "svelte3/typescript": true, 
+  },
+};
