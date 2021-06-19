ff6c075cfc95c291d488920e1dc5a7ee84290c67
Sebastian Martinez <sebastinez@me.com>
2021-06-19 00:18:03 -0300

    Add IDX as user identity provider
    Signed-off-by: Sebastian Martinez <sebastinez@me.com>
    Signed-off-by: Njoku Emmanuel <kalunjoku123@gmail.com>

---
+++ src/base/idx/Link.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import type { Config } from '@app/config';
+  import { Caip10Link } from '@ceramicnetwork/stream-caip10-link';
+  import { EthereumAuthProvider } from '@ceramicnetwork/blockchain-utils-linking';
+
+  export let did: string;
+  export let caip10: string;
+  export let config: Config;
+  
+  onMount(async () => {
+    if (config.signer) {
+      const address = await config.signer.getAddress();
+      const ethAuthProvider = new EthereumAuthProvider(
+        // TODO: Using the window.ethereum provider here since the config provider is throwing error.
+        window.ethereum, address
+      );
+
+      const accountLink = await Caip10Link.fromAccount(config.ceramic.client, caip10.toLowerCase());
+      await accountLink.setDid(
+        did,
+        ethAuthProvider
+      );
+
+    }
+  });
+</script>
