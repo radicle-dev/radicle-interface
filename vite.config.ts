8d2e05f9a8a468ea3e2e3665070294ffbbca67f7
Alexis Sellier <self@cloudhead.io>
2021-06-20 17:29:55 +0200

    Fix paths with a "." in them not loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import path from "path";
 import { UserConfig } from "vite";
 import svelte from "@sveltejs/vite-plugin-svelte";
+import rewriteAll from "vite-plugin-rewrite-all";
 
 const config: UserConfig = {
   optimizeDeps:{
     exclude:['svelte-routing']
   },
-  plugins: [svelte()],
+  plugins: [svelte(), rewriteAll()],
   resolve: {
     alias: {
       "@app": path.resolve("./src"),
