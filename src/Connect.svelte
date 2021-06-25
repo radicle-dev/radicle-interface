9c99186f2539c3ea7cd5e0ba26bf5772ae5bba37
Njoku Emmanuel <kalunjoku123@gmail.com>
2021-06-25 13:26:19 +0100

    Implement wallet connect client
    Signed-off-by: Njoku Emmanuel <kalunjoku123@gmail.com>

--- src/Connect.svelte
+++ src/Connect.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { Connection } from "@app/session";
   import { state } from "@app/session";
+  import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+
   import WCEthereumProvider from "@app/WalletConnectProvider";
   import type { Config } from "@app/config";
   import Modal from "@app/Modal.svelte";
@@ -13,28 +16,52 @@
 
   let isModalOpen = false;
   console.log();
-  const TEST_SESSION_CHAIN_ID = config.network.chainId;
+  // const TEST_SESSION_CHAIN_ID = 3;
 
-  const TEST_SESSION_RPC_HOST =
-    "https://rinkeby.infura.io/v3/e9c4665d91a343e295308d5995ff5a72";
+  // const TEST_SESSION_RPC_HOST =
+  //   "https://rinkeby.infura.io/v3/e9c4665d91a343e295308d5995ff5a72";
 
-  const TEST_PROVIDER_OPTS = {
-    chainId: TEST_SESSION_CHAIN_ID,
-    qrcode: true,
-    bridge: "https://staging.walletconnect.org",
-    rpc: {
-      [TEST_SESSION_CHAIN_ID]: TEST_SESSION_RPC_HOST,
-    },
-  };
+    // Create a connector
+const connector = new WalletConnect({
+  bridge: "https://bridge.walletconnect.org", // Required
+  qrcodeModal: QRCodeModal,
+});
+  // const TEST_PROVIDER_OPTS = {
+  //   chainId: TEST_SESSION_CHAIN_ID,
+  //   qrcode: true,
+  //   bridge: "https://staging.walletconnect.org",
+  //   rpc: {
+  //     [TEST_SESSION_CHAIN_ID]: TEST_SESSION_RPC_HOST,
+  //   },
+  // };
+
 
   const onClickConnect = async () => {
     //isModalOpen = !isModalOpen;
 
-    const provider = new WCEthereumProvider(TEST_PROVIDER_OPTS);
+    // const provider = new WCEthereumProvider(TEST_PROVIDER_OPTS);
 
-    const providerAccounts = await provider.enable();
-    // state.connect(config);
-    console.log(providerAccounts, "provider accounts");
+    // const providerAccounts = await provider.enable();
+    // // state.connect(config);
+    // console.log(providerAccounts, "provider accounts");
+
+    // Check if connection is already established
+if (!connector.connected) {
+  // create new session
+  connector.createSession();
+}
+
+// Subscribe to connection events
+connector.on("connect", (error, payload) => {
+  if (error) {
+    throw error;
+  }
+
+  // Get provided accounts and chainId
+  const { accounts, chainId } = payload.params[0];
+
+  console.log(accounts, chainId)
+});
   };
 
   $: connecting = $state.connection === Connection.Connecting;
