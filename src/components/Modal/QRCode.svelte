b980785c49218c36d2f860d33b1122cc950ad04c
Njoku Emmanuel <kalunjoku123@gmail.com>
2021-07-05 22:09:52 +0100

    Implement custom qrcode modal UI
    Signed-off-by: Njoku Emmanuel <kalunjoku123@gmail.com>

---
+++ src/components/Modal/QRCode.svelte
@@ -0,0 +1,42 @@
+<script lang="typescript">
+    import { qrcode } from "pure-svg-code";
+    import Modal from "@app/Modal.svelte";
+
+    export let uri: string;
+  
+    $: svgString = qrcode({
+      content: uri,
+      width: 225,
+      height: 225,
+      color: "black",
+      background: "white",
+      ecl: "M",
+    });
+  </script>
+  
+  <Modal floating={true}>
+    <p slot="title">Connect your wallet</p>
+    <p slot="subtitile" style="text-align: center;">
+      Scan this code with your wallet. Not working? <br />
+      <a href="https://walletconnect.org/wallets" class="typo-link">
+        View compatible wallets.
+      </a>
+    </p>
+  
+    <div slot="body" class="qrcode-wrapper">
+      <div data-cy="qr-code">
+        {@html svgString}
+      </div>
+    </div>
+  </Modal>
+  
+  <style>
+    .qrcode-wrapper {
+      width: fit-content;
+      margin: var(--content-padding) auto;
+      padding: calc(var(--content-padding) / 2);
+      border-radius: 1rem;
+      background-color: white;
+    }
+  </style>
+  
