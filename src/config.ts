3eb000e19e39c89159a33563e85f85e36de4dbc8
Njoku Emmanuel <kalunjoku123@gmail.com>
2021-07-10 11:22:21 +0000

    Chore: refactor getConfig to allow WalletConnect
    Signed-off-by: Njoku Emmanuel <kalunjoku123@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,9 @@ import type { TypedDataSigner } from '@ethersproject/abstract-signer';
 import SafeServiceClient from "@gnosis.pm/safe-service-client";
 import CeramicClient from "@ceramicnetwork/http-client";
 import { IDX } from "@ceramicstudio/idx";
+import WalletConnect from "@walletconnect/client";
 import config from "@app/config.json";
-import type { WalletConnectSigner } from "./WalletConnectSigner";
+import { WalletConnectSigner } from "./WalletConnectSigner";
 
 declare global {
   interface Window {
@@ -42,11 +43,9 @@ export class Config {
   ) {
     const cfg = (<Record<string, any>> config)[network.name];
     const api = config.radicle.api;
-
     if (!cfg) {
       throw `Network ${network.name} is not supported`;
     }
-
     const ceramic = new CeramicClient(config.ceramic.api);
     const idx = new IDX({ ceramic });
 
@@ -80,16 +79,55 @@ const gasLimits = {
   createOrg: 1_200_000,
 };
 
+let walletConnect: WalletConnect;
+
 function isMetamaskInstalled(): boolean {
   const { ethereum } = window;
   return Boolean(ethereum && ethereum.isMetaMask);
+}
+
+function isWalletConnectConnected(): boolean {
+  const newWalletConnect = (): WalletConnect => {
+    return new WalletConnect({
+      bridge: "https://bridge.walletconnect.org",
+    });
+  };
+  walletConnect = newWalletConnect();
+
+  if (walletConnect.connected){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 export async function getConfig(): Promise<Config> {
   let config: Config;
   const alchemyApiKey = import.meta.env.RADICLE_ALCHEMY_API_KEY;
 
-  if (isMetamaskInstalled()) {
+  const disconnect = async () => {
+    await walletConnect.killSession().catch(() => {
+      // When the user disconnects wallet-side, calling `killSession`
+      // app-side trows an error because the wallet has already closed
+      // its socket. Therefore, we simply ignore it.
+    });
+  };
+
+  if (isWalletConnectConnected()){
+    //ethereum provider
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // instantiate wallet connect signer
+    const signer = new WalletConnectSigner(walletConnect, provider, disconnect);
+
+    const provNetwork = await ethers.providers.getNetwork(
+      signer.walletConnect.chainId
+    );
+    const network = {
+      name: provNetwork.name,
+      chainId: provNetwork.chainId,
+    };
+    config = new Config(network, provider, signer);
+  } else if (isMetamaskInstalled()) {
     // If we have Metamask, use it as the signer, but try to use Alchemy
     // as the provider.
     const metamask = new ethers.providers.Web3Provider(window.ethereum);
