795d6c43f8bb600d895c638969f41a2dbd16aa2c
Rūdolfs Ošiņš <rudolfs@osins.org>
2022-08-25 15:14:18 +0200

    Require `let` or `const` instead of var
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- src/polyfills/typedarray-to-buffer.js
+++ src/polyfills/typedarray-to-buffer.js
@@ -7,7 +7,7 @@ import { Buffer } from 'buffer/';
 
 export default function typedarrayToBuffer(arr) {
   if (isTypedArray.strict(arr)) {
-    var buf = Buffer.from(arr.buffer);
+    let buf = Buffer.from(arr.buffer);
     if (arr.byteLength !== arr.buffer.byteLength) {
       buf = buf.slice(arr.byteOffset, arr.byteOffset + arr.byteLength);
     }
