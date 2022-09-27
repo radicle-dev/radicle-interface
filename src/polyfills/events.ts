d86eb2ee288f664b092d67a97f437abb6089cb58
Rūdolfs Ošiņš <rudolfs@osins.org>
2022-09-27 10:23:40 +0200

    Remove unused polyfills
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- src/polyfills/events.ts
+++
@@ -1,5 +0,0 @@
-// This shim is used as a stand-in for the node "events" library, used by
-// the "keccack" package depended on by @gnosis.pm/safe-core-sdk.
-import events from "events";
-
-export default events;
--- src/polyfills/fetch.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-import fetch from "node-fetch";
-
-if (!globalThis.fetch) {
-  globalThis.fetch = fetch;
-  globalThis.Headers = fetch.Headers;
-  globalThis.Request = fetch.Request;
-  globalThis.Response = fetch.Response;
-}
