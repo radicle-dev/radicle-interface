7db68e81d0b9f7e6ccffcebcc4806fa82e004752
Rūdolfs Ošiņš <rudolfs@osins.org>
2022-09-27 10:23:40 +0200

    Remove unused polyfills
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- src/polyfills/canvas.js
+++
@@ -1,7 +0,0 @@
-// A simple mock for some HTMLCanvasElement functions to run in a node env.
-
-HTMLCanvasElement.prototype.getContext = () => ({
-  fillRect: () => null,
-});
-
-HTMLCanvasElement.prototype.toDataURL = () => null;
