dab1770a13ef5e2b097afe54176452ee2d56d3de
Rūdolfs Ošiņš <rudolfs@osins.org>
2022-09-29 15:13:13 +0200

    Retry a test twice before giving up
    We have some flaky tests in the test suite. This should take care of it.
    
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   video: false,
   defaultCommandTimeout: 10000,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
   e2e: {
     specPattern: "cypress/e2e/**/*spec.ts",
     baseUrl: "http://localhost:3000",
