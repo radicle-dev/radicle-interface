d8ae56f62a92ca05a3561cf1f0eff57edee06b4d
Rūdolfs Ošiņš <rudolfs@osins.org>
2022-10-27 11:38:24 +0200

    Add Playwright dependency
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

---
+++ playwright.config.ts
@@ -0,0 +1,36 @@
+import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  testDir: "./tests/e2e",
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "list",
+  use: {
+    actionTimeout: 0,
+    baseURL: "http://localhost:3000",
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
+  ],
+
+  webServer: {
+    command: "npm run start",
+    port: 3000,
+  },
+};
+
+export default config;
