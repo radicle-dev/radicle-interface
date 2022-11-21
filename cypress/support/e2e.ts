0c9f71d93b63ef92006c401c2643c62ef683f106
Rūdolfs Ošiņš <rudolfs@osins.org>
2022-11-21 17:20:26 +0100

    Remove cypress
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- cypress/support/e2e.ts
+++
@@ -1,6 +0,0 @@
-function getPath(location: Location): string {
-  const url = location.href.replace(window.origin, "");
-  return window.HASH_ROUTING ? url.substring(2) : url;
-}
-
-export { getPath };
