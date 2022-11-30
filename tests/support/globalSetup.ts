8818f3816a733066e941fd0fa72a13a40df402f7
Rūdolfs Ošiņš <rudolfs@osins.org>
2022-11-30 14:47:54 +0100

    Allow starting local test seed without Docker
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- tests/support/globalSetup.ts
+++ tests/support/globalSetup.ts
@@ -1,25 +1,33 @@
 import type { FullConfig } from "@playwright/test";
 
-import { exec } from "child_process";
-
 export default async function globalSetup(_config: FullConfig): Promise<void> {
   assertHttpApiRunning();
 }
 
-// Assert that the docker container with the test http-api is running. If it is
-// not running, throw an error that explains how to run it.
+// Assert that the test http-api is running. If it is not running, throw an
+// error that explains how to run it.
 async function assertHttpApiRunning(): Promise<void> {
-  const containerName = "radicle-http-api-test";
-  const notRunningMessage =
-    "The http-api test container is required for this test.\n" +
-    "👉 You can run it with `./scripts/http-api-test`\n";
+  const palmTestFixtureSeedId =
+    "hyb6i8oggc3mgra9siy8yuohhtz34r98pcybja97c9o789wpsg6nn4";
 
-  exec(
-    `docker container inspect ${containerName} --format {{.State.Running}}`,
-    (_error, stdout, _stderr) => {
-      if (stdout.trim() !== "true") {
-        throw new Error(notRunningMessage);
-      }
-    },
-  );
+  const notRunningMessage =
+    "The http-api server with test fixtures needs to be running.\n" +
+    "👉 You can start it with `./scripts/http-api-test`\n";
+
+  let peerId: string | undefined = undefined;
+
+  try {
+    const response = await fetch("http://0.0.0.0:8777");
+    const data = await response.json();
+    peerId = data.peer.id;
+  } catch (err) {
+    console.error(err);
+    throw new Error(notRunningMessage);
+  }
+
+  if (peerId !== palmTestFixtureSeedId) {
+    const wrongSeedMessage =
+      "The server on port 8777 doesn't have the right fixtures.\n";
+    throw new Error(wrongSeedMessage + notRunningMessage);
+  }
 }
