85dc1c3552558b6bafd5df3dbe8750a3b6381cb7
Rūdolfs Ošiņš <rudolfs@osins.org>
2023-05-03 16:43:40 +0200

    Port mutexExecutor from Upstream
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

---
+++ src/lib/mutexExecutor.ts
@@ -0,0 +1,112 @@
+// Copyright © 2021 The Radicle Upstream Contributors
+//
+// This file is part of radicle-upstream, distributed under the GPLv3
+// with Radicle Linking Exception. For full terms see the included
+// LICENSE file.
+
+import * as Bacon from "baconjs";
+
+// A task executor that runs only one task concurrently. If a new task
+// is run, any previously running task is aborted and the promise
+// returned from `run()` will return undefined.
+//
+//     import * as mutexExecutor from "ui/src/mutexExecutor"
+//     const executor = mutexExecutor.create()
+//     const first = await executor.run(async () => {
+//       await sleep(1000)
+//       return "first"
+//     })
+//     const second = await executor.run(async () => "second")
+//
+// In the example above the promise `first` will resolve to `undefined`
+// while the promise `second` will resolve to "second".
+//
+// If the first tasks throws after the second task has run the
+// behavior is the same.
+//
+//     const first = await executor.run(async () => {
+//       await sleep(1000)
+//       throw new Error()
+//     })
+//
+// The task call back receives an AbortSignal as a parameter. The abort
+// event is emitted when another task is run.
+export function create(): MutexExecutor {
+  return new MutexExecutor();
+}
+
+// A worker that asynchronously process one item at a time and provides
+// the result as an event stream.
+//
+//     import * as mutexExecutor from "ui/src/mutexExecutor"
+//     const worker = mutexExecutor.createWorker(async (value) => {
+//       await sleep(1000)
+//       return value
+//     })
+//
+//     const firstPromise = worker.output.firstToPromise()
+//     worker.push("first)
+//     assert.equal(await firstPromise, "first")
+//
+// When an item is submitted to the worker while the previous items is
+// still being processed, the result of the first item will not be
+// emitted to `worker.output`. Instead, only the last item will be
+// emitted.
+export function createWorker<In, Out>(
+  fn: (x: In, abortSignal: AbortSignal) => Promise<Out>,
+): MutexWorker<In, Out> {
+  return new MutexWorker(fn);
+}
+
+class MutexExecutor {
+  private runningTaskId = 0;
+  private abortController: AbortController | null = null;
+
+  public async run<T>(
+    f: (abortSignal: AbortSignal) => Promise<T>,
+  ): Promise<T | undefined> {
+    this.runningTaskId += 1;
+    const taskId = this.runningTaskId;
+
+    if (this.abortController) {
+      this.abortController.abort();
+    }
+    this.abortController = new AbortController();
+    return f(this.abortController.signal).then(
+      data => {
+        if (this.runningTaskId === taskId) {
+          return data;
+        } else {
+          return undefined;
+        }
+      },
+      err => {
+        if (this.runningTaskId === taskId) {
+          throw err;
+        } else {
+          return undefined;
+        }
+      },
+    );
+  }
+}
+
+class MutexWorker<In, Out> {
+  private outputBus = new Bacon.Bus<Out>();
+  private executor = new MutexExecutor();
+
+  public output: Bacon.EventStream<Out>;
+
+  public constructor(
+    private fn: (x: In, abortSignal: AbortSignal) => Promise<Out>,
+  ) {
+    this.output = this.outputBus.toEventStream();
+  }
+
+  public async submit(x: In): Promise<void> {
+    const output = await this.executor.run(abort => this.fn(x, abort));
+    if (output !== undefined) {
+      this.outputBus.push(output);
+    }
+  }
+}
---
+++ tests/unit/mutexExecutor.test.ts
@@ -0,0 +1,122 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import * as sinon from "sinon";
+import { describe, expect, test } from "vitest";
+
+import * as mutexExecutor from "@app/lib/mutexExecutor";
+import { sleep } from "@app/lib/sleep";
+
+describe("executor", () => {
+  test("cancels running task", async () => {
+    const e = mutexExecutor.create();
+
+    const first = e.run(async () => {
+      await sleep(10);
+      return "first";
+    });
+    const second = e.run(async () => {
+      return "second";
+    });
+
+    expect(await first).toBe(undefined);
+    expect(await second).toBe("second");
+
+    const third = e.run(async () => {
+      await sleep(10);
+      return "third";
+    });
+    const fourth = e.run(async () => {
+      return "fourth";
+    });
+
+    expect(await third).toBe(undefined);
+    expect(await fourth).toBe("fourth");
+  });
+
+  test("cancels multiple tasks", async () => {
+    const e = mutexExecutor.create();
+
+    const canceled1 = e.run(async () => {
+      await sleep(10);
+      return true;
+    });
+    const canceled2 = e.run(async () => {
+      await sleep(10);
+      return true;
+    });
+    const canceled3 = e.run(async () => {
+      await sleep(10);
+      return true;
+    });
+    const last = e.run(async () => {
+      return true;
+    });
+
+    expect(await canceled1).toBe(undefined);
+    expect(await canceled2).toBe(undefined);
+    expect(await canceled3).toBe(undefined);
+    expect(await last).toBe(true);
+  });
+
+  test("triggers abort signal event", async () => {
+    const e = mutexExecutor.create();
+    const abortListener = sinon.spy();
+
+    e.run(async abort => {
+      abort.addEventListener("abort", abortListener);
+      await sleep(10);
+      return "first";
+    });
+    expect(abortListener.called).toBe(false);
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    e.run(async () => {});
+    expect(abortListener.called).toBe(true);
+  });
+
+  test("don’t throw error on aborted task", async () => {
+    const e = mutexExecutor.create();
+
+    const first = e.run(async () => {
+      await sleep(10);
+      throw new Error();
+    });
+    const second = e.run(async () => {
+      return "second";
+    });
+
+    expect(await first).toBe(undefined);
+    expect(await second).toBe("second");
+  });
+});
+
+describe("worker", () => {
+  test("sequential work", async () => {
+    const w = mutexExecutor.createWorker(async (value: number) => {
+      await sleep(10);
+      return value;
+    });
+
+    const outputs: number[] = [];
+    w.output.onValue(value => outputs.push(value));
+
+    await w.submit(1);
+    await w.submit(2);
+    await w.submit(3);
+
+    expect(outputs).toEqual([1, 2, 3]);
+  });
+
+  test("overlapping work cancels", async () => {
+    const w = mutexExecutor.createWorker(async (value: number) => {
+      await sleep(10);
+      return value;
+    });
+
+    const nextOutput = w.output.firstToPromise();
+
+    w.submit(1);
+    w.submit(2);
+    w.submit(3);
+
+    expect(await nextOutput).toEqual(3);
+  });
+});
