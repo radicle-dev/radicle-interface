dbad9ddeefb911dc9e88bf113152d8c442ada8d4
Sebastian Martinez <me@sebastinez.dev>
2023-06-30 07:24:30 +0200

    Remove hack for `happy-dom`
    The update to happy-dom 8.0 did remove the need to do the assignment
    hack in `setupVitest`
    
    source: https://github.com/vitest-dev/vitest/issues/2305#issuecomment-1396387954
    
    Signed-off-by: Sebastian Martinez <me@sebastinez.dev>

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   test: {
-    setupFiles: "./tests/support/setupVitest",
     environment: "happy-dom",
     include: ["tests/unit/**/*.test.ts"],
     reporters: "verbose",
