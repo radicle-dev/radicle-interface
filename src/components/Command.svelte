18f9b0e8c7242bb38b909a44ee49eab3ca6f93b0
Rūdolfs Ošiņš <rudolfs@osins.org>
2023-09-01 11:38:16 +0200

    Make whole command clickable
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- src/components/Command.svelte
+++ src/components/Command.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import { SvelteComponent } from "svelte";
+
   import Clipboard from "@app/components/Clipboard.svelte";
 
   export let command: string;
+
+  let clipboard: SvelteComponent;
 </script>
 
 <style>
@@ -9,6 +13,7 @@
     display: flex;
   }
   .cmd {
+    cursor: pointer;
     height: 2rem;
     line-height: 2rem;
     background-color: var(--color-foreground-3);
@@ -24,6 +29,14 @@
     white-space: nowrap;
     border: 1px solid var(--color-border-hint);
     color: var(--color-foreground-dim);
+    user-select: none;
+  }
+  .cmd:hover {
+    border: 1px solid var(--color-border-default);
+    color: var(--color-foreground-contrast);
+  }
+  .cmd:hover .clipboard {
+    background-color: var(--color-fill-ghost-hover);
   }
   .clipboard {
     display: flex;
@@ -36,16 +49,19 @@
     width: 2rem;
     height: 100%;
   }
-  .clipboard:hover {
-    background-color: var(--color-fill-ghost-hover);
-  }
 </style>
 
 <div class="wrapper">
-  <div class="cmd">
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div
+    class="cmd"
+    on:click={() => {
+      clipboard.copy();
+    }}>
     {command}
     <div class="clipboard">
-      <Clipboard small text={command} />
+      <Clipboard bind:this={clipboard} small text={command} />
     </div>
   </div>
 </div>
