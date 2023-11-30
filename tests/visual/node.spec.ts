e080883059990c62f52f957f27229b6cac95da86
Sebastian Martinez <me@sebastinez.dev>
2023-11-30 22:27:24 +0100

    Add private project visual tests

--- tests/visual/node.spec.ts
+++ tests/visual/node.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@tests/support/fixtures.js";
+import { createProject } from "@tests/support/project.js";
 
 test("node page", async ({ page }) => {
   await page.addInitScript(() => {
@@ -12,6 +13,21 @@ test("node page", async ({ page }) => {
   });
 
   await page.goto("/nodes/radicle.local", { waitUntil: "networkidle" });
+  await expect(page).toHaveScreenshot();
+});
+
+test("private projects", async ({ page, authenticatedPeer }) => {
+  await createProject(authenticatedPeer, {
+    name: "private-project",
+    visibility: "private",
+  });
+  await page.goto("/", { waitUntil: "networkidle" });
+  await expect(page).toHaveScreenshot();
+
+  await page.goto(authenticatedPeer.uiUrl(), { waitUntil: "networkidle" });
+  await expect(page).toHaveScreenshot();
+
+  await page.getByRole("link", { name: "private-project Private" }).click();
   await expect(page).toHaveScreenshot();
 });
 
