9224d42ec4719ea2892f3586bf07105bcbb19417
dnlklmn <pelicansinberlin@gmail.com>
2024-02-05 22:06:18 +0100

    Make rid lighter on hover

--- src/components/CopyableId.svelte
+++ src/components/CopyableId.svelte
@@ -11,12 +11,15 @@
 <style>
   .id {
     cursor: pointer;
-    color: var(--color-fill-secondary);
+    color: var(--color-foreground-emphasized);
     overflow-wrap: anywhere;
     display: flex;
     align-items: center;
     gap: 0.125rem;
     width: fit-content;
+  }
+  .id:hover {
+    color: var(--color-foreground-emphasized-hover);
   }
 </style>
 
@@ -31,7 +34,8 @@
   <span
     class="txt-overflow"
     class:global-commit={style === "commit"}
-    class:global-oid={style === "oid"}>
+    class:global-oid={style === "oid"}
+    style="color: inherit">
     <slot>{id}</slot>
   </span>
   <Clipboard bind:this={clipboard} text={id} />
