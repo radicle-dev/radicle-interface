e475596ec0992299a4103e03fe42842ce362dc59
Sebastian Martinez <me@sebastinez.dev>
2024-04-16 11:40:09 +0200

    Remove private project visual test
    Currently the httpd doesn't serve private projects, so this test fails
    everytime.
    We should look at it again once we figured out how to serve private
    projects securely.

--- tests/visual/desktop/node.spec.ts
+++ tests/visual/desktop/node.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from "@tests/support/fixtures.js";
-import { createProject } from "@tests/support/project.js";
 
 test("node page", async ({ page }) => {
   await page.addInitScript(() => {
@@ -13,21 +12,6 @@ test("node page", async ({ page }) => {
   });
 
   await page.goto("/nodes/radicle.local", { waitUntil: "networkidle" });
-  await expect(page).toHaveScreenshot();
-});
-
-test("private projects", async ({ page, authenticatedPeer }) => {
-  await createProject(authenticatedPeer, {
-    name: "private-project",
-    visibility: "private",
-  });
-  await page.goto("/", { waitUntil: "networkidle" });
-  await expect(page).toHaveScreenshot();
-
-  await page.goto(authenticatedPeer.uiUrl(), { waitUntil: "networkidle" });
-  await expect(page).toHaveScreenshot();
-
-  await page.getByRole("link", { name: "private-project Private" }).click();
   await expect(page).toHaveScreenshot();
 });
 
