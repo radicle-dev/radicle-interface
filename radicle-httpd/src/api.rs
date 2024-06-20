473f893b2b967c4d5cc32dee5f888622831f7129
Sebastian Martinez <me@sebastinez.dev>
2024-06-20 12:23:19 +0200

    http: release radicle-httpd v0.11.0

--- radicle-httpd/src/api.rs
+++ radicle-httpd/src/api.rs
@@ -36,7 +36,7 @@ use crate::Options;
 
 pub const RADICLE_VERSION: &str = env!("RADICLE_VERSION");
 // This version has to be updated on every breaking change to the radicle-httpd API.
-pub const API_VERSION: &str = "0.1.0";
+pub const API_VERSION: &str = "1.0.0";
 
 /// Identifier for sessions
 type SessionId = String;
