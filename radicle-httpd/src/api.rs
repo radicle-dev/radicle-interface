b39389c1a1afccba00405e7082ad218461a08863
Sebastian Martinez <me@sebastinez.dev>
2024-06-28 10:21:00 +0200

    http: Add repo policy endpoint

--- radicle-httpd/src/api.rs
+++ radicle-httpd/src/api.rs
@@ -36,7 +36,7 @@ use crate::Options;
 
 pub const RADICLE_VERSION: &str = env!("RADICLE_VERSION");
 // This version has to be updated on every breaking change to the radicle-httpd API.
-pub const API_VERSION: &str = "1.0.0";
+pub const API_VERSION: &str = "1.1.0";
 
 /// Identifier for sessions
 type SessionId = String;
