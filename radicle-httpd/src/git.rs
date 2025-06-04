radicle-dev/radicle-interface

commit 5f22e1bb7727ba8f6689e3b882609d6a9505657d

httpd/git: Set `uploadpack.allowAnySHA1InWant`

radicle-httpd/src/git.rs
@@ -106,6 +106,10 @@ async fn git_http_backend(
     let mut cmd = Command::new("git");
     let mut child = cmd
         .arg("http-backend")
+        // This is a workaround to allow fetching particular commits by their OID.
+        // Otherwise, the client errors with "Server does not allow request for unadvertised object"
+        // See also `REF_UNADVERTISED_NOT_ALLOWED` in Git's `fetch-pack.c` (as of 0bd2d79).
+        .args(["-c", "uploadpack.allowAnySHA1InWant"])
         .env("REQUEST_METHOD", method.as_str())
         .env("GIT_PROJECT_ROOT", git_dir)
         // "The GIT_HTTP_EXPORT_ALL environmental variable may be passed to git-http-backend to bypass

commit ccb5e49ee0f445d2c90d20d4deec529d824aea33

httpd/git: Fix passing configuration
The `-c` argument (and its corresponding value) must come *before* the
first subcommand to be effective. This error was introduced in commit
5f22e1bb7727ba8f6689e3b882609d6a9505657d.

radicle-httpd/src/git.rs
@@ -105,11 +105,11 @@ async fn git_http_backend(
 
     let mut cmd = Command::new("git");
     let mut child = cmd
-        .arg("http-backend")
         // This is a workaround to allow fetching particular commits by their OID.
         // Otherwise, the client errors with "Server does not allow request for unadvertised object"
         // See also `REF_UNADVERTISED_NOT_ALLOWED` in Git's `fetch-pack.c` (as of 0bd2d79).
         .args(["-c", "uploadpack.allowAnySHA1InWant"])
+        .arg("http-backend")
         .env("REQUEST_METHOD", method.as_str())
         .env("GIT_PROJECT_ROOT", git_dir)
         // "The GIT_HTTP_EXPORT_ALL environmental variable may be passed to git-http-backend to bypass